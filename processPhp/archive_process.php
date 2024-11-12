0cf90f22c01477d86c829da9ae83348b0635e709
Karl Satchi Navida <satchi5600@gmail.com>
2024-11-12 16:30:43 +0800

    Refactor 20241112 - Category Page Refactor
    Refactored the entire Category Table for parity to the other tables implemented from earlier changes and commits.
    
    [CHANGELOG]
    🟢 Added a temporary TODO.
    🟢 Added a prepared statement  creator function for edit process file.
    🟢 Added new CSS class that adds the "important" marker denoted by a red asterisk.
    🟡 Refactored the category table in the category page.
    🟡 Refactored the code for processing archiving in the archive process file.
    🟡 Refactored the code for restoring archived items in the archive process file.
    🟡 Refactored the editing process in the edit process file.
    🔴 Remvoed more unused codes.

--- processPhp/archive_process.php
+++ processPhp/archive_process.php
@@ -1,83 +1,15 @@
 <?php
 require_once("../connection.php");
 
-// ARCHIVE MAIN PRODUCT
-if (isset($_POST['action']) && $_POST['action'] == "archivemainProduct") {
-	$productId = $_POST['id'];
-	$archive = "Yes";
-	$sqlu = "UPDATE products SET archive='$archive' WHERE product_Id='$productId'";
-	$resultu = $conn->query($sqlu);
-	if ($resultu) {
-		echo "successArchive";
-	} else {
-		echo "errorArchive";
-	}
-}
-
-// ARCHIVE CATEGORY
-if (isset($_POST['action']) && $_POST['action'] == "archiveCategory") {
-	$categoryId = $_POST['id'];
-	$archive = "Yes";
-	$sqlu = "UPDATE category_table SET archive='$archive' WHERE category_Id='$categoryId'";
-	$resultu = $conn->query($sqlu);
-	if ($resultu) {
-		echo "successArchive";
-	} else {
-		echo "errorArchive";
-	}
-}
-
-// ARCHIVE PRODUCT
-if (isset($_POST['action']) && $_POST['action'] == "archiveProduct") {
-	$productId = $_POST['id'];
-	$archive = "Yes";
-	$sqlu = "UPDATE category_product_table SET archive='$archive' WHERE category_product_Id='$productId'";
-	$resultu = $conn->query($sqlu);
-	if ($resultu) {
-		echo "successArchive";
-	} else {
-		echo "errorArchive";
-	}
-}
-
-// ARCHIVE PRODUCT TYPE
-if (isset($_POST['action']) && $_POST['action'] == "archiveProductType") {
-	$productTypeId = $_POST['id'];
-	$archive = "Yes";
-	$sqlu = "UPDATE category_product_item_table SET archive='$archive' WHERE category_product_item_Id='$productTypeId'";
-	$resultu = $conn->query($sqlu);
-	if ($resultu) {
-		echo "successArchive";
-	} else {
-		echo "errorArchive";
-	}
-}
-
-// ARCHIVE TYPE
-if (isset($_POST['action']) && $_POST['action'] == "archiveType") {
-	$typeId = $_POST['id'];
-	$archive = "Yes";
-	$sqlu = "UPDATE category_product_item_type_table SET archive='$archive' WHERE category_product_item_type_Id='$typeId'";
-	$resultu = $conn->query($sqlu);
-	if ($resultu) {
-		echo "successArchive";
-	} else {
-		echo "errorArchive";
-	}
-}
-
-// RESTORATION PART //
-
-// Restore Category
 if (isset($_POST['action'])) {
 	$action = $_POST['action'];
 	/**
 	 * Contains the list of actions. Its key is the action name and while its value is an array containing
 	 * the table name, id column name, and the archive column name respectively, allowing for easy retrieval
 	 * of the table and id column name.
-	 * @var array $actions
+	 * @var array $restoreActions
 	 */
-	$actions = [
+	$restoreActions = [
 		"restoreCategory" => [
 			"table" => "category_table",
 			"id" => "category_Id",
@@ -115,11 +47,11 @@ if (isset($_POST['action'])) {
 		],
 	];
 
-	if (array_key_exists($action, $actions)) {
-		$table = $actions[$action]['table'];
-		$idColumn = $actions[$action]['id'];
+	if (array_key_exists($action, $restoreActions)) {
+		$table = $restoreActions[$action]['table'];
+		$idColumn = $restoreActions[$action]['id'];
 		$id = $_POST['id'];
-		$targetCol = $actions[$action]['targetCol'];
+		$targetCol = $restoreActions[$action]['targetCol'];
 		$archive = $targetCol == "archive" ? "No" : "Active";
 
 		$sql = "UPDATE `$table` SET `$targetCol` = '$archive' WHERE `$idColumn` = '$id'";
@@ -130,4 +62,6 @@ if (isset($_POST['action'])) {
 		else
 			echo "error";
 	}
+
+	// TODO: Implement the archiveActions below
 }
